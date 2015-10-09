39ebb4f01de277dcbd86b246d48f0f412fefbf09
vee879 <veeshah4@gmail.com>
2015-10-09 17:29:35 +1100

    Footer updated

--- wp-content/themes/onlineclassifieds/footer.php
+++ wp-content/themes/onlineclassifieds/footer.php
@@ -48,7 +48,7 @@
 						echo $footer_copyright;
 					} else {
 				?>
-				 &#x00040; 2014 Classify - by <a class="target-blank" href="http://themeforest.net/user/joinwebs">Joinwebs</a>
+				 &#x00040; 2014 Online Classifieds - by <a class="target-blank" href="http://52.26.21.8/">Group 3</a>
 				<?php } ?>
 				
 			</div><!-- .site-info -->
@@ -65,6 +65,5 @@
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 	<?php wp_footer(); ?>
-	<p class="TK">Powered by <a href="http://themekiller.com/" title="themekiller" rel="follow"> themekiller.com </a><a href="http://anime4online.com/" title="themekiller" rel="follow"> anime4online.com </a> <a href="http://animextoon.com/" title="themekiller" rel="follow"> animextoon.com </a> </p>
 </body>
 </html>
