1fee46c34f549ed9475b61d90001a807a54e9c03
Vee879 <veeshah4@gmail.com>
2015-10-20 09:51:05 +0000

    Update wp-content

---
+++ wp-content/cache/db/000000/all/799/685/799685a8f44d214db843c316871e7913.php
@@ -0,0 +1,6 @@
+=&V<?php exit; ?>a:6:{s:10:"last_error";s:0:"";s:10:"last_query";s:110:"
+					SELECT term_id
+					FROM wp_term_taxonomy
+					WHERE taxonomy = 'category'
+					AND term_id IN (25)
+				";s:11:"last_result";a:1:{i:0;O:8:"stdClass":1:{s:7:"term_id";s:2:"25";}}s:8:"col_info";a:1:{i:0;O:8:"stdClass":13:{s:4:"name";s:7:"term_id";s:7:"orgname";s:7:"term_id";s:5:"table";s:16:"wp_term_taxonomy";s:8:"orgtable";s:16:"wp_term_taxonomy";s:3:"def";s:0:"";s:2:"db";s:8:"Group3DB";s:7:"catalog";s:3:"def";s:10:"max_length";i:2;s:6:"length";i:20;s:9:"charsetnr";i:63;s:5:"flags";i:49193;s:4:"type";i:8;s:8:"decimals";i:0;}}s:8:"num_rows";i:1;s:10:"return_val";i:1;}
